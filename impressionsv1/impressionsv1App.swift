6827060975081c09e7b4fce8b8297f58cec72798
Hardik Golchha <hardikgolchha0089@gmail.com>
2026-02-07 14:48:26 +0530

    Initial Commit

---
+++ impressionsv1/impressionsv1App.swift
@@ -0,0 +1,32 @@
+//
+//  impressionsv1App.swift
+//  impressionsv1
+//
+//  Created by Hardik Golchha on 07/02/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct impressionsv1App: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ impressionsv1Tests/impressionsv1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  impressionsv1Tests.swift
+//  impressionsv1Tests
+//
+//  Created by Hardik Golchha on 07/02/26.
+//
+
+import Testing
+@testable import impressionsv1
+
+struct impressionsv1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
